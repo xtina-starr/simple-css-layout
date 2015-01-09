c0d35bd37693fbc7f7c82d5b428b409b79f2b93c
Christina Thompson <c.thompsonms@gmail.com>
2015-01-09 12:53:24 -0800

    added an event lister to launch a popup dialogue box with favorite things and create an element in #section

--- main.js
+++ main.js
@@ -13,4 +13,38 @@ window.onload = function() {
     var element = document.getElementById('modal');
     element.style.visibility = 'hidden';
   };
+
+  document.getElementById('me').addEventListener('click', favoriteThings);
+
+  var newDiv = document.createElement('div');
+  newDiv.innerHTML = '<ul><li>Music</li><li>culture</li><li>Nina Simone</li></ul>';
+
+  var contentEle = document.getElementsByClassName('content-container')[0];
+  var article = document.getElementsByClassName('text-content')[0];
+  contentEle.insertBefore(newDiv, article);
 };
+
+function favoriteThings() {
+  var arr = ['music', 'culture', 'Nina Simone', 'traveling'];
+  var arrSize = arr.length;
+  var str = 'My favorite things are ';
+
+  for (var i = 0;  i < arrSize; i++) {
+    if (i === (arrSize - 1)) {
+      str = str + '& ' + arr[i] + '.';
+    } else {
+      str = str + arr[i] + ', ';  
+    };
+  };
+  alert(str);
+
+  
+};
+
+// var newDiv = document.createElement('div');
+// newDiv.innerHTML = '<ul><li>Music</li><li>culture</li><li>Nina Simone</li></ul>';
+
+// var header = document.getElementByTagName('header');
+// header.appendChild(newDiv);
+
+
